74373506ca55f786a8e29232e62c1c4828714049
Erne <ernestodainesiperrot@gmail.com>
2020-07-07 20:07:37 -0300

    defined crear & destruir

--- heap.c
+++ heap.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include "heap.h"
+#define TAM_INI 10
 
 /* *****************************************************************
  *                        ESTRUCTURAS
@@ -23,11 +24,30 @@ struct heap{
  *                    PRIMITIVAS DEL HEAP
  * *****************************************************************/
 
-heap_t *heap_crear(cmp_func_t cmp);
+heap_t *heap_crear(cmp_func_t cmp){
+    heap_t* heap = malloc(sizeof(heap_t));
+    if(!heap) return NULL;
+    heap->datos = malloc(sizeof(void*) * TAM_INI);
+    if(!heap->datos){
+        free(heap);
+        return NULL;
+    }
+    heap->cant = 0;
+    heap->tam = TAM_INI;
+    heap->cmp = cmp;
+    return heap;
+}
 
 heap_t *heap_crear_arr(void *arreglo[], size_t n, cmp_func_t cmp);
 
-void heap_destruir(heap_t *heap, void (*destruir_elemento)(void *e));
+void heap_destruir(heap_t *heap, void (*destruir_elemento)(void *e)){
+    for(int i = 0; i < heap->tam; i++){
+        if(destruir_elemento)
+            destruir_elemento(heap->datos[i]);
+    }
+    free(heap->datos);
+    free(heap);
+}
 
 size_t heap_cantidad(const heap_t *heap){
     return heap->cant;
@@ -43,7 +63,7 @@ void *heap_ver_max(const heap_t *heap){
     if(heap_esta_vacio(heap)){
         return NULL;
     }
-    return heap->datos[0]; // esto nose si es asi, pero relleno para las pruebas.
+    return heap->datos[0];
 }
 
 void *heap_desencolar(heap_t *heap);
