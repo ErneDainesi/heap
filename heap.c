35b3b234233318c608f51dae01d52838e621f43b
Erne <ernestodainesiperrot@gmail.com>
2020-07-07 21:19:13 -0300

    added upheap & downheap

--- heap.c
+++ heap.c
@@ -18,13 +18,44 @@ struct heap{
 /* *****************************************************************
  *                    FUNCIONES AUXILIARES
  * *****************************************************************/
-void swap(int *x, int *y) {
-    int aux = *x;
-    *x = *y;
-    *y = aux;
+void swap(void *x, void *y) {
+    void* aux = x;
+    x = y;
+    y = aux;
 }
 
-// upheap, downheap
+int calcular_pos_padre(int hijo){
+    return (hijo - 1) / 2;
+}
+
+int calcular_max(heap_t* heap, cmp_func_t cmp, int padre, int hijo_izq, int hijo_der){
+    if( (cmp(heap->datos[padre], heap->datos[hijo_izq]) >= 0) && (cmp(heap->datos[padre], heap->datos[hijo_der]) >= 0) )
+        return padre;
+    if( (cmp(heap->datos[hijo_izq], heap->datos[padre]) >= 0) && (cmp(heap->datos[hijo_izq], heap->datos[hijo_der]) >= 0) )
+        return hijo_izq;
+    if( (cmp(heap->datos[hijo_der], heap->datos[padre]) >= 0) && (cmp(heap->datos[hijo_der], heap->datos[hijo_izq]) >= 0) )
+        return hijo_der;
+}
+
+void upheap(heap_t* heap, int hijo, cmp_func_t cmp){
+    if(hijo == 0) return;
+    int padre = calcular_pos_padre(hijo);
+    if(cmp(heap->datos[padre], heap->datos[hijo]) < 0){
+        swap(heap->datos[padre], heap->datos[hijo]);
+        upheap(heap, padre, cmp);
+    }
+}
+
+void downheap(heap_t* heap, size_t tam, int padre, cmp_func_t cmp){
+    if(padre >= tam - 1) return;
+    int hijo_izq = 2 * padre + 1;
+    int hijo_der = 2 * padre + 2;
+    int max = calcular_max(heap, cmp, padre, hijo_izq, hijo_der);
+    if(cmp(heap->datos[max], heap->datos[padre]) != 0){
+        swap(heap->datos[padre], heap->datos[max]);
+        downheap(heap, tam, max, cmp);
+    }
+}
 
 /* *****************************************************************
  *                    PRIMITIVAS DEL HEAP
